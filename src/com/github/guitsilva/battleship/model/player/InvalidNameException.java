f6a0d44422503a1fe0671ff38629bbf59795ab3a
Guilherme Tavares <guilherme.tsilva@gmail.com>
2021-12-23 16:09:25 -0300

    feat: add player model

---
+++ src/com/github/guitsilva/battleship/model/player/InvalidNameException.java
@@ -0,0 +1,8 @@
+package com.github.guitsilva.battleship.model.player;
+
+public class InvalidNameException extends RuntimeException {
+
+  public InvalidNameException(String message) {
+    super(message);
+  }
+}
