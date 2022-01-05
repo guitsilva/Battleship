7daf7fa00823f7be6f388384f9c437ce08516028
Guilherme Tavares <guilherme.tsilva@gmail.com>
2022-01-04 22:18:01 -0300

    feat: add frames package

--- src/com/github/guitsilva/battleship/Main.java
+++ src/com/github/guitsilva/battleship/Main.java
@@ -1,7 +1,12 @@
 package com.github.guitsilva.battleship;
 
+import com.github.guitsilva.battleship.view.frames.TitleFrame;
+
 public class Main {
 
   public static void main(String[] args) {
+    TitleFrame titleFrame = new TitleFrame();
+
+    titleFrame.render();
   }
 }
