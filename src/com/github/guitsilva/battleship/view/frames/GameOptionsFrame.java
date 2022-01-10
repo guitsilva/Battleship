d137f57a4478946f43e9e0a1b9aaa79b84e3a3c2
Daniel <danieltostes@live.com>
2022-01-10 13:37:43 -0300

    feat: main menu navigation with frames

--- src/com/github/guitsilva/battleship/view/frames/GameOptionsFrame.java
+++ src/com/github/guitsilva/battleship/view/frames/GameOptionsFrame.java
@@ -2,17 +2,18 @@ package com.github.guitsilva.battleship.view.frames;
 
 import com.github.guitsilva.battleship.view.Console;
 
-public class TitleFrame implements Frame {
+
+public class GameOptionsFrame implements Frame {
 
   public void render() {
     Console.clear();
     // IMPORTANT: 25 lines x 100 columns
     Console.print("Options:", 100, true, ' ');
     Console.print("");
+    Console.print("(N) Set player name", 100, true, ' ');
+    Console.print("(G) Set game mode", 100, true, ' ');
     Console.print("(P) Play", 100, true, ' ');
     Console.print("(Q) Quit", 100, true, ' ');
-    Console.print("");
-    Console.print("");
     Console.print("");
     Console.print("");
     Console.print("");
